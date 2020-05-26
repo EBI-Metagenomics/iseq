8ce832b6128b6a862a6b8bb81e249489f42219b3
Danilo Horta <danilo.horta@pm.me>
2020-05-26 13:06:16 +0100

    remove pkgresources

--- iseq/_cli/_cli.py
+++ iseq/_cli/_cli.py
@@ -1,11 +1,21 @@
 import click
 
-from ._misc import get_version
 from .gff import gff_dedup
 from .press import press
 from .scan import scan
 from .score import score
 from .unpress import unpress
+
+
+def get_version():
+    import re
+    import iseq
+    import importlib_resources as pkg_resources
+
+    content = pkg_resources.read_text(iseq, "__init__.py")
+    c = re.compile(r"__version__ *= *('[^']+'|\"[^\"]+\")")
+    m = c.search(content)
+    return m.groups()[0][1:-1]
 
 
 @click.group(name="iseq", context_settings=dict(help_option_names=["-h", "--help"]))
