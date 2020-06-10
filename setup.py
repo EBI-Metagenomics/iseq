b2c6aec0e8b9d7fcc3f4b80a790279f780045996
Danilo Horta <danilo.horta@pm.me>
2020-06-10 11:24:03 +0100

    pyproject

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 if __name__ == "__main__":
-    console_scripts = ["iseq = iseq:cli"]
-    setup(entry_points={"console_scripts": console_scripts},)
+    setup()
