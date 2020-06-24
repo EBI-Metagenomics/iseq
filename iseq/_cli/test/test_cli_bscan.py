f921aac9560470b5a3b3581cded1ef8f792e4c6c
Danilo Horta <danilo.horta@pm.me>
2020-06-24 15:26:33 +0100

    actual bscan test

--- iseq/_cli/test/test_cli_bscan.py
+++ iseq/_cli/test/test_cli_bscan.py
@@ -31,10 +31,13 @@ def test_cli_bscan_GALNBKIG_pfam10(tmp_path):
     )
     assert r.exit_code == 0, r.output
 
+    r = invoke(cli, ["press", str(profile)])
+    assert r.exit_code == 0, r.output
+
     r = invoke(
         cli,
         [
-            "pscan",
+            "bscan",
             str(profile),
             str(fasta),
             "--output",
@@ -44,6 +47,62 @@ def test_cli_bscan_GALNBKIG_pfam10(tmp_path):
             "--oamino",
             "bscan_oamino.fasta",
             "--quiet",
+        ],
+    )
+    assert r.exit_code == 0, r.output
+
+    assert cmp("bscan_oamino.fasta", "pscan_oamino.fasta", shallow=False), diff(
+        "bscan_oamino.fasta", "pscan_oamino.fasta"
+    )
+    assert cmp("bscan_ocodon.fasta", "pscan_ocodon.fasta", shallow=False), diff(
+        "bscan_ocodon.fasta", "pscan_ocodon.fasta"
+    )
+    assert cmp("bscan_output.gff", "pscan_output.gff", shallow=False), diff(
+        "bscan_output.gff", "pscan_output.gff"
+    )
+
+
+def test_cli_bscan_GALNBKIG_pfam10_ncpus2(tmp_path):
+    os.chdir(tmp_path)
+    invoke = CliRunner().invoke
+    profile = example_filepath("Pfam-A.33.1_10.hmm")
+    fasta = example_filepath("GALNBKIG_00914_ont_01_plus_strand.fasta")
+
+    r = invoke(
+        cli,
+        [
+            "pscan",
+            str(profile),
+            str(fasta),
+            "--output",
+            "pscan_output.gff",
+            "--ocodon",
+            "pscan_ocodon.fasta",
+            "--oamino",
+            "pscan_oamino.fasta",
+            "--quiet",
+        ],
+    )
+    assert r.exit_code == 0, r.output
+
+    r = invoke(cli, ["press", str(profile)])
+    assert r.exit_code == 0, r.output
+
+    r = invoke(
+        cli,
+        [
+            "bscan",
+            str(profile),
+            str(fasta),
+            "--output",
+            "bscan_output.gff",
+            "--ocodon",
+            "bscan_ocodon.fasta",
+            "--oamino",
+            "bscan_oamino.fasta",
+            "--quiet",
+            "--ncpus",
+            2,
         ],
     )
     assert r.exit_code == 0, r.output
