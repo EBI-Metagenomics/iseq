85408151ea88e89e55eb24897528ed72d479a03b
Danilo Horta <danilo.horta@pm.me>
2020-07-14 19:36:57 +0100

    rm scripts

--- profmark/run_hmmer.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-hmmscan --cut_ga --domtblout domtblout.txt Pfam-A_small.hmm AE014075.1_amino.fasta
--- profmark/run_iseq.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-iseq pscan2 Pfam-A_small.hmm AE014075.1_nucl.fasta --model 2
-iseq gff-filter output.gff --max-e-value 1e-10 > output2.gff
