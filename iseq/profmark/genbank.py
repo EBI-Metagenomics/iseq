5cf1e35cf51a28049f8503eee068ecd6bdacd472
Danilo Horta <danilo.horta@gmail.com>
2020-08-07 11:46:37 +0100

    fix on first codon when not a starting codon

--- iseq/profmark/genbank.py
+++ iseq/profmark/genbank.py
@@ -162,13 +162,10 @@ def remove_stop_codon(nucl_seq: Seq, amino_seq: Seq, trans_table_num: int):
         stop = min(start + 3, len(nucl_str))
         codon = nmm.Codon.create(nucl_str[start:stop].encode(), base_abc)
         if start == 0:
-            # Add M to the first position, no matter
-            # what the nucleotide sequence says.
-            # That is what genbank seems to be doing.
-            if codon not in codon_table.start_codons:
-                print("Warning: the first codon does not code for M. Adding M anyway.")
-            aminos.append("M")
-            continue
+            if codon in codon_table.start_codons:
+                # GenBank seems to be doing this.
+                aminos.append("M")
+                continue
         aminos.append(codon_table.decode(codon).decode())
 
     amino_str = "".join(aminos)
